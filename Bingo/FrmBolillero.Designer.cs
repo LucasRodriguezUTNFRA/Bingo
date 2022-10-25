4df7aec543e835ebb2d107ee61911b3d5e5611f6
Lucas Rodriguez <104538567+LucasRodriguezUTNFRA@users.noreply.github.com>
2022-10-25 14:16:22 -0300

    Add files via upload

---
+++ Bingo/FrmBolillero.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Bingo2E
+{
+    partial class FrmBolillero
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_numero = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_numero
+            // 
+            this.btn_numero.Location = new System.Drawing.Point(30, 310);
+            this.btn_numero.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_numero.Name = "btn_numero";
+            this.btn_numero.Size = new System.Drawing.Size(178, 77);
+            this.btn_numero.TabIndex = 0;
+            this.btn_numero.Text = "Sacar proximo numero";
+            this.btn_numero.UseVisualStyleBackColor = true;
+            this.btn_numero.Click += new System.EventHandler(this.btn_numero_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(30, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N° Cantados";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(30, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 232);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(30, 410);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ultimo Numero";
+            // 
+            // FrmBolillero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 447);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_numero);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmBolillero";
+            this.Text = "Bolillero";
+            this.Load += new System.EventHandler(this.FrmBolillero_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_numero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Bingo/FrmBolillero.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Bingo2E
+{
+
+    public partial class FrmBolillero : Form
+    {
+        static Random rnd;
+        List<int> numerosDisponibles;
+        List<int> numerosCantados;
+
+        public FrmBolillero()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            numerosDisponibles = new List<int>();
+            numerosCantados = new List<int>();
+            SetearEstadoInicial();
+        }
+
+        private void SetearEstadoInicial()
+        {
+            numerosCantados.Clear();
+
+            for (int i = 0; i < 100; i++)
+            {
+                numerosDisponibles.Add(i);
+            }
+        }
+
+        private int ObtenerNumero()
+        {
+            int numeroQueSalio = rnd.Next(0, numerosDisponibles.Count);
+
+            numeroQueSalio = numerosDisponibles[numeroQueSalio];
+
+            numerosDisponibles.Remove(numeroQueSalio);
+
+            numerosCantados.Add(numeroQueSalio);
+
+            return numeroQueSalio;
+        }
+
+        private void btn_numero_Click(object sender, EventArgs e)
+        {
+            int numero = ObtenerNumero();
+            label2.Text = "Ultimo Numero:" + numero.ToString();
+
+        }
+
+        public void CantarBingo(string nombre)
+        {
+            MessageBox.Show("Ganador!!! : " + nombre);
+
+            this.btn_numero.Enabled = false;
+
+        }
+
+
+        private void FrmBolillero_Load(object sender, EventArgs e)
+        {
+
+            new FrmCarton("pepe").Show();
+            new FrmCarton("Juana").Show();
+            new FrmCarton("Caro").Show();
+
+        }
+    }
+}
